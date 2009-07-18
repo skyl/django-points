b604f11b88e5033fb7f16af25b358034b3b2e076
biouser <skylar.saveland@gmail.com>
2009-07-18 12:47:54 -0400

    Add and change should be solid for AJAX and non

--- urls.py
+++ urls.py
@@ -3,37 +3,21 @@ from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list
 
 urlpatterns = patterns('',
-    url(r'^$', view='points.views.all', name="points_list"),
+    url(r'^$', view='points.views.list', name="points_list"),
     url(r'^(?P<id>\d+)/$', view='points.views.detail', name='points_detail'),
 
     url(r'^delete/(?P<id>\d+)/', view='points.views.delete', name='points_delete'),
 
-    url(r'change/(?P<id>\d+)/', view='points.views.form', name='points_change'),
+    url(r'change/(?P<id>\d+)/', view='points.views.change', name='points_change'),
 
 
-    # FIXME if the slug is just numbers this would be bad
-    #url(r'(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/(?P<id>\d+)/$',\
-    #            view='point_list', name='points_list'),
+    url(r'list/(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/(?P<id>\d+)/$',\
+            view='points.views.list', name='points_list'),
 
-    #url(r'(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/(?P<slug>[-\w]+)/$',\
-    #            view='point_list', name='points_list'),
+    url(r'list/(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/$',\
+            view='points.views.list', name='points_list'),
 
-    #url(r'(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/$',\
-    #            view='point_list', name='points_list'),
+    url(r'add/(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/(?P<id>\d+)/$',\
+            view='points.views.add', name='points_add'),
 
-
-
-    #url(r'(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/(?P<id>\d+)/add/$',\
-    #            view='point_form', name='points_form'),
-    #url(r'(?P<app_label>[-\w]+)/(?P<model_name>[-\w]+)/(?P<slug>[-\w]+)/add/$',\
-    #             view='point_form', name='points_form'),
-
-
-
-
-
-    #url(r'^terms/$', direct_to_template, {"template": "about/terms.html"}, name="terms"),
-    #url(r'^privacy/$', direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
-    #url(r'^dmca/$', direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
-    #url(r'^what_next/$', direct_to_template, {"template": "about/what_next.html"}, name="what_next"),
 )
--- views.py
+++ views.py
@@ -13,15 +13,66 @@ from points.models import Point
 from django.core import serializers
 
 
-def all(request):
-    points = Point.objects.all()
+def list(request, app_label=None, model_name=None, id=None):
+    ''' List all points (ALL, all for model instance, or all for table)
+
+    I wanted to overload a view for fun so as to be more dry but all of this
+    logic might be wetter still FIXME?
+    '''
+
+    if not id and not model_name and not app_label:
+        points = Point.objects.all()
+
+    elif id and model_name and app_label:
+        try:
+            ct = ContentType.objects.get(\
+                    app_label = app_label,
+                    model = model_name)
+
+            obj = ct.get_object_for_this_type( id=id )
+
+            points = Point.objects.filter( content_object=obj )
+
+        except:
+            # FIXME, does this look good?  In need of a convention...
+            if request.is_ajax():
+                return HttpResponse(status=500)
+            else:
+                return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+        points = Point.objects.filter(content_object = obj)
+        context = {'points':points, 'object':obj, 'content_type':ct, }
+
+    elif app_label and model_name and not id:
+        try:
+            ct = ContentType.objects.get(\
+                    app_label = point.content_type.app_label,
+                    model = point.content_type.model)
+        except:
+            # FIXME, does this look good?  In need of a convention...
+            # and, this could be a bit more DRY?
+            if request.is_ajax():
+                return HttpResponse(status=500)
+            else:
+                return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+        points = Point.objects.filter(content_type = ct)
+        context = {'points':points, 'content_type':ct, }
+
+    else:
+        # FIXME seriously, this might not be cool..
+        if request.is_ajax():
+            return HttpResponse(status=500)
+
+        else:
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
 
     if request.is_ajax():
         return HttpResponse(serializers.serialize("json",points),\
                 mimetype='application/javascript')
 
     else:
-        context = {'points':points, }
         return render_to_response('points/all.html', context,\
                 context_instance=RequestContext(request))
 
@@ -32,16 +83,17 @@ def detail(request, id):
 
     try:
         point = Point.objects.get(id=id)
+
     except:
         return HttpResponseRedirect(reverse('points_list'))
 
-    t = ContentType.objects.get(\
+    ct = ContentType.objects.get(\
             app_label = point.content_type.app_label,
             model = point.content_type.model)
 
-    o = t.get_object_for_this_type(id = point.object_id)
+    obj = ct.get_object_for_this_type(id = point.object_id)
 
-    context = {'point':point, 'object':o , }
+    context = {'point':point, 'object':obj, 'content_type': ct,  }
 
     if request.is_ajax():
         return HttpResponse(serializers.serialize("json", point),
@@ -59,7 +111,13 @@ def delete(request, id):
     try:
         point = Point.objects.get(id=id)
     except:
-        return HttpResponseRedirect(reverse('points_list'))
+        # FIXME?
+        if request.is_ajax():
+            return HttpResponse(status=500)
+
+        else:
+            #return HttpResponseRedirect(reverse('points_list'))
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
     context = {'point':point,}
     if request.user == point.owner:
@@ -80,11 +138,9 @@ def delete(request, id):
     else:
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
-
-
 @login_required
-def form(request, id):
-    ''' Change the point location for a Point '''
+def change(request, id):
+    ''' Change the data for a single Point() obj '''
 
     try:
         point = Point.objects.get(id=id)
@@ -93,43 +149,10 @@ def form(request, id):
 
     if request.method == 'POST' and point.owner == request.user:
 
+        # FIXME? What needs to be posted with ajax to make a valid form?
+        # I would hope that you could just post 1 field and the others
+        # would stay the same.  Think that is the way that it would work.
         form = PointForm(request.POST, instance=point)
-
-        if form.is_valid():
-            if request.is_ajax():
-                form.save()
-                return HttpResponse(status=201)
-            else:
-                form.save()
-                return HttpResponseRedirect(request.META['HTTP_REFERER']) 
-
-    elif point.owner == request.user:
-        form = PointForm( instance=point )
-        context = {'point':point, 'form':form}
-        return render_to_response('points/change.html', context,\
-                context_instance=RequestContext(request) )
-
-    else:
-        return HttpResponseRedirect(request.META['HTTP_REFERAL'])
-'''
-
-Below is scratch, not documentation.
-
-    one can supply id-only to change an existing Point or, alter
-    print request.META['HTTP_REFERER']
-
-    if id and not app_label and not model_name and not slug:
-        if request.method == 'POST':
-            try:
-                point = Point.objects.get(id=id)
-            except:
-                return HttpResponseRedirect(request.META['HTTP_REFERER'])
-
-    if request.method == 'POST':
-        if point:
-            form = PointForm(request.POST, instance = point)
-        else:
-            form = PointForm(request.POST)
 
         if form.is_valid():
             form.save()
@@ -138,12 +161,60 @@ Below is scratch, not documentation.
             else:
                 return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
-    else:
-        if point:
-            context = {'point':point,}
-        else:
-            context = {}
+    elif point.owner == request.user:
+        form = PointForm( instance=point )
+        context = {'point':point, 'form':form, }
+        return render_to_response('points/change.html', context,\
+                context_instance=RequestContext(request) )
 
-        return render_to_response('points/point_form.html', context,\
-                context_instance = RequestContext(request) )
-'''
+    else:
+        return HttpResponseRedirect(request.META['HTTP_REFERAL'])
+
+@login_required
+def add(request, app_label, model_name, id):
+    '''add a point to a content_object
+
+    POST the point and the zoom.  The owner is request.user and the related
+    obj is received from the url.
+    '''
+    try:
+        ct = ContentType.objects.get(\
+                app_label = app_label,
+                model = model_name)
+        obj = ct.get_object_for_this_type( id=id )
+
+    except:
+        if request.is_ajax():
+            # FIXME? is this the right thing to return?
+            return HttpResponse(status=500)
+
+        else:
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+    if request.method == 'POST':
+        form = PointForm(request.POST)
+
+        if form.is_valid():
+            # FIXME? do we need to try/except this suite?
+            form.save(commit=False)
+            form.owner = request.user
+            form.object_id = id
+            form.content_type = ct
+            form.content_obj = obj
+            form.save()
+
+            # FIXME? is this really a way that we can handle the redirect?
+            if request.is_ajax():
+                return HttpResponse(status=201)
+
+            else:
+                return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+    else:
+        form = PointForm()
+        context = {'form':form, 'object':obj, 'content_type':ct, }
+
+        return render_to_response('points/add.html', context,\
+                context_instance = RequestContext(request))
+
+
