b69fb3523dbfff7f37c4cb1ef3e2f47e138744cf
Skylar Saveland <skylar.saveland@gmail.com>
2009-07-26 05:08:44 -0400

    google_maps fail, {% load Point_tags %} now

--- templatetags/point_tags.py
+++ templatetags/point_tags.py
@@ -1,10 +1,12 @@
 from django import template
-
 register = template.Library()
 
+from django.contrib.contenttypes.models import ContentType
+
+from points.models import Point
 
 @register.inclusion_tag('points/link.html')
-def add_point_link(model_instance, css_class=None):
+def add_point_link(model_instance, css_class="add-point"):
     '''Inclusion tag to rendering simple link to the point form page
 
     arguments: model_instance, css_class
@@ -15,6 +17,14 @@ def add_point_link(model_instance, css_class=None):
 
     return locals()
 
+@register.inclusion_tag('points/show_google_map.html')
+def show_google_map(model_instance, css_id="default-map"):
+    ct = ContentType.objects.get_for_model(model_instance)
+    obj_id = model_instance.id
+    if Point.objects.filter(content_type = ct, object_id=obj_id):
+        point = Point.objects.filter(content_type = ct, object_id=obj_id)[0]
+
+    return locals()
 
 '''
 # FIXME? right now I am trying to just be able to {% include ajax_jquery.html %}
