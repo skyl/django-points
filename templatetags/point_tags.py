32618b38c197346e63c40ba6048b6ae728e908f3
Skylar Saveland <skylar.saveland@gmail.com>
2009-07-26 22:15:45 -0400

    setting up pure ol defaults

--- templatetags/point_tags.py
+++ templatetags/point_tags.py
@@ -2,6 +2,7 @@ from django import template
 register = template.Library()
 
 from django.contrib.contenttypes.models import ContentType
+from olwidget.widgets import MapDisplay
 
 from points.models import Point
 
@@ -22,9 +23,12 @@ def show_google_map(model_instance, css_id="default-map"):
     ''' Turn a DOM element into a map using the google API
 
     {% show_google_map model_instance css_id%}
+    Note that this returns the *latest* point that has been
+    added to the model instance.
     '''
     ct = ContentType.objects.get_for_model(model_instance)
     obj_id = model_instance.id
+
     if Point.objects.filter(content_type = ct, object_id=obj_id):
         point = Point.objects.filter(content_type = ct, object_id=obj_id)[0]
     else:
@@ -32,16 +36,29 @@ def show_google_map(model_instance, css_id="default-map"):
 
     return locals()
 
-'''
-# FIXME? right now I am trying to just be able to {% include ajax_jquery.html %}
+@register.inclusion_tag('points/show_ol_map.html')
+def show_ol_map(model_instance):
+    ''' Takes a model instance and returns display of all points html
 
-@register.inclusion_tag('points/add_form_ajax.html')
-def add_point_ajax(css_class):
-    '' Add the javascript necessary to ui-dialog the point form
+    '''
+    ct = ContentType.objects.get_for_model(model_instance)
+    obj_id = model_instance.id
 
-    {% add_point_ajax css_class %}
+    points = Point.objects.filter(content_type = ct, object_id=obj_id)
 
-    ''
+    map = MapDisplay( fields=[p.point for p in points],
+            map_options = {
+                    'map_style':{'width':'260px', 'height':'180px',},
+                    'layers': ['osm.mapnik',],
+            }
+    )
+
 
     return locals()
-'''
+
+@register.inclusion_tag('points/show_ol_media.html')
+def show_ol_media(model_instance):
+    ''' What needs to be in head for show_ol_map tag
+
+    '''
+    show_ol_map(model_instance)
